ce0d2b504380e0a313c66a01350dd4b500bde0f5
Farhan Ali <farhanyounas5204@gmail.com>
2023-07-11 14:33:09 +0500

    Add files via upload

---
+++ variablesForBooleans.js
@@ -0,0 +1,26 @@
+// var isfeepaid= true;
+// var exampassed= true;
+
+// alert(isfeepaid);
+
+// var  age = 30;
+// age = undefined;
+// console.log(age);
+
+// var age = 30;
+console.log(age);
+age = "Farhan";
+console.log(age);
+age=true;
+console.log(age);
+typeof age;
+
+var Name= "Farhan";
+var age = 24;
+var isfeepaid = false;
+ console.log(typeof Name);
+ console.log(typeof age);
+ console.log(typeof isfeepaid);
+
+var a=10; var b = 20; var c=30;
+alert(a); alert(b);

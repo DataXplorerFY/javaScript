6674e8c4671e25c57a50b66ad92e09eda5e15edd
Farhan Ali <farhanyounas5204@gmail.com>
2023-07-17 02:19:18 +0500

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,62 @@
+/* Q1: Write a script to display the following message on your web page:(Hint : Use line break)
+Message "welcome to javascript land happy coding!"
+
+console.log("Happy coding");
+alert("welcome to javascript land \n Happy coding!");
+Document.write(); */
+
+
+/* Q2:Write a script to save student’s bio data in JS variables and show the data in alert boxes
+var Name= "Henry";
+var age = 28;
+var course=" Web 3.0 metaverse"
+
+alert("Student Name is :"+ Name);
+alert("Student age is: "+ age);
+alert("Student enrolled course:"+ course);
+
+console.log("Student Name is :",Name);
+console.log("Student age is: ",age);
+console.log("Student enrolled course:", course);*/
+
+/*Q3: Declare a variable called email and assign to it a string that
+represents your Email Address(e.g. example@example.com). Show
+the below mentioned message in an alert box.(Hint: use string
+concatenation)
+
+var email= "Farhanyounas5204@gmail.com";
+alert("My email adress is "+ email);*/
+
+/*Q4:Write a script to display this in browser through JS
+var message = "Yah! I can write HTML content through JavaScript";
+document.write(message);*/
+
+/*Q5:Declare a variable called age & assign to it your age. Show your age
+in an alert box.
+
+var age = 24;
+alert("i am " + age + " Years old");*/
+
+/*Q6: Declare a variable called birthYear & assign to it your birth year.
+Show the following message in your browser:
+
+var birthYear= 2002;
+document.write("My birthyear is"+ " " + birthYear + "<br> Data type of by birthyear is Number ");*/
+
+/*Q7:Display this in your browser
+a. A heading stating “Rules for naming JS variables”
+b. Variable names can only contain ______, ______,
+c. ______ and ______.
+d. For example $my_1stVariable.
+e. Variables must begin with a ______, ______ or
+f. _____. For example $name, _name or name
+g. Variable names are case _________
+h. Variable names should not be JS _________
+
+document.write("<h1>Rules For naming variables in js</h1>")
+
+document.write("Variable names can only contain letters, _ numbers,and $ ");
+document.write("<br>For example $my_1stVariable.");
+document.write("<br>Variables must begin with a letter, _, or, <br> $. For example $name, _name or name");
+document.write("<br>Variable names are case sensitive");
+document.write("<br>Variable names should not be JS keywords")*/

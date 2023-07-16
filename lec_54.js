e4ff2290c94ad01c267ce313791d075ba1c2638a
Farhan Ali <farhanyounas5204@gmail.com>
2023-07-16 23:01:44 +0500

    Add files via upload

---
+++ lec_54.js
@@ -0,0 +1,25 @@
+//alert("Hello World!")
+// console.log("This is Farhan!")
+
+// document.write("This is document.write functions information")
+
+var Name= "Farhan";
+var age= 23;
+var height= 5.8;
+console.log( "My Name is", Name, "And my Age is", age, "My height is", height);
+
+var marks1=40;
+var marks2=30;
+var marks3=60;
+console.log("My Totall marks of my 6th semester is ",marks1+marks2+marks3);
+
+var firstname="Farhan ";
+
+var lastname="Ali";
+
+console.log("My full name is ",firstname+  lastname);
+
+var isfeepaid= true;
+console.log(isfeepaid);
+
+console.log("typof is fee paid is",typeof isfeepaid);

a16fb00fb12e8e66c24fb36c50e15f5588610601
Farhan Ali <farhanyounas5204@gmail.com>
2023-07-17 12:34:09 +0500

    Add files via upload

---
+++ practice_2.js
@@ -0,0 +1,116 @@
+// variables 
+var age=24;
+var Name= "Farhan";
+var height = 5.8;
+
+var isFeepaid = true;
+var exampassed= false;
+
+var fee;
+var lastname=null;
+
+console.log(age)
+console.log(Name)
+console.log(height)
+console.log(isFeepaid)
+console.log(exampassed)
+console.log(fee)
+console.log(lastname)
+
+
+// Case Sensitive
+var rose="Hi";
+var Rose="Hello";
+var RoSE="Okay";
+console.log(rose);
+console.log(Rose);
+console.log(RoSE);
+
+// Arithmatic operators:
+
+var a = 5;
+var b = 3;
+
+var c = (a+b); // addition
+var d = (a-b);// subtraction
+var e = (a*b);// multiplication
+var f = (a/b);// division
+var g = (a%b);// modulos
+var h = (a**b);// exponent
+
+console.log("Addition is ",c);
+console.log("Subtraction is ",d);
+console.log("Multiplication is ",e);
+console.log("Division is ",f);
+console.log("modulos is ",g);
+console.log("exponent is ",h);
+
+
+var age=25;
+var newAge= age + 25;
+console.log("My new age is ",newAge);
+
+// Assignment operators:
+a=10;
+a +=20;
+console.log("a=a+20 is ",a);
+a -=20;
+console.log("a=a-20 is ",a);
+a *=20;
+console.log("a=a*20 is ",a);
+a /=20;
+console.log("a=a/20 is ",a);
+a %= 20;
+console.log("a=a%20 is ",a);
+a **=20;
+console.log("a=a**20 is ",a);
+
+
+// Write a JavaScript expression that calculates the sum of two variables a and b and assigns the result to a variable sum.
+
+var a = 10;
+var b = 20;
+var sum = (a+b);
+console.log("The sum of the above two varibales are",sum);
+
+// What is the difference between the addition operator (+) and the assignment addition operator (+=) in JavaScript? Provide an example to illustrate the difference.
+
+
+var one= 10;
+var two = 20;
+var Sum= (one + two ); // it will sum the two numbers;
+var assignmentOperator = two += one; // it will return the vaue thirty
+
+console.log(Sum);
+console.log(assignmentOperator);
+
+// Write a JavaScript expression that calculates the product of two variables x and y and assigns the result to a variable product.
+var x = 10;
+var y = 20;
+var product= (x*y);
+console.log("Product of X ans Y is ",product);
+
+// What is the purpose of the modulo operator (%) in JavaScript? Provide an example that demonstrates its usage.
+
+var X= 20;
+var Y= 30;
+var C= ( X%Y);
+console.log(C);
+
+//Write a JavaScript expression that increments a variable count by 1 using both the increment operator (++) and the assignment addition operator (+=).
+var A= 10;
+A++;
+console.log("Increment is ",A)
+
+console.log("Assignment operator is ",A+=A);
+
+// Write a JavaScript expression that calculates the remainder when dividing a variable dividend by another variable divisor, and assigns the result to a variable remainder.
+
+var dividend = 42;
+var divisor = 7;
+var remainder = dividend % divisor;
+ console.log(remainder);
+
+
+
+

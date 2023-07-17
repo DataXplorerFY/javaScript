1c402201321ad256f405185c4ed01a3e2d346fb3
Farhan Ali <farhanyounas5204@gmail.com>
2023-07-17 14:23:24 +0500

    Add files via upload

---
+++ textString.js
@@ -0,0 +1,22 @@
+var Name= "Farhan";
+var age = 24;
+var Nameage= (Name+" "+age);
+console.log(Nameage);
+console.log(typeof Nameage);
+
+
+var a = "6"+2;
+console.log(a)
+
+var age=prompt("What is your age?") // if user dont click on it it will be show by default 26
+console.log(age);
+
+var newAge= age +  5;
+console.log(newAge);
+
+// Convert string to integer:
+
+var age = prompt("What is your age?");
+var num = 5.5;
+var sum = Number(age)+ Number(num);
+console.log(sum);

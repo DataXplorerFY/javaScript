d9324ec894ec74df4f38e6fe431dac43ebf6196e
Farhan Ali <farhanyounas5204@gmail.com>
2023-07-20 22:13:32 +0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,51 @@
+// var names=["farhan","asad","ali","Uzair","talha"];
+// var userInput=prompt("Enter name that you want to find!");
+// console.log("User Input:"+userInput );
+// var found=false;
+// for(i=0; i<names.length; i++){
+//     if (names[i]===userInput){
+//         console.log("Found "+userInput+" on index = "+i);
+//         found=true;
+//         break;
+//     }
+// }
+// if(!found){
+//     console.log("Not found");
+// }
+
+
+
+
+// var books_array = [
+//     "The Great Gatsby",
+//     "To Kill a Mockingbird",
+//     "1984",
+//     "Pride and Prejudice",
+//     "Harry Potter and the Sorcerer's Stone",
+//     "The Lord of the Rings",
+//     "The Catcher in the Rye",
+//     "The Hobbit",
+//     "Brave New World",
+//     "Fahrenheit 451"
+// ]
+
+// var userinput=prompt("Enter book name!");
+// console.log("user input: "+ userinput);
+// var found=false
+// for(i=0; i<books_array.length; i++){
+//     if(books_array[i]===userinput){
+//         console.log("Found "+ userinput + " on index = "+ i);
+//         found=true;
+//         break;
+//     }
+// }
+// if(!found){
+//     console.log("Sorry Not found!")
+// }
+
+var numbers=[11,22,33,44,55,66,77,88,99];
+var input=parseInt(prompt("Enter thye number you want!"));
+console.log("User input: " + input);
+for(i=0; i<numbers.length ; i++){
+    
+}

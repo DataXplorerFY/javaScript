2a2d6f77721e65c84e9c6120c69a695dd29a8d1e
Farhan Ali <farhanyounas5204@gmail.com>
2023-07-24 23:22:31 +0500

    Add files via upload

---
+++ js-Assignment 3.js
@@ -0,0 +1,51 @@
+/*Declare and Initialize an array and store available education
+qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M.
+   Phil., PhD). Show the listed qualifications in your browser like:
+
+document.write("Qualifications<br>");
+var Pakistan=["SSC", "HSC","BCS","BS","BCOM","MS","M.Phil","PhD"];
+for(i=0; i<Pakistan.length ; i++){
+    document.write("<br>",Pakistan[i]);
+}
+*/
+
+
+// Write a program to store 3 student names in an array.Take another
+// array to store score of these three students. Assume that total marks
+// are 500 for each student, display the scores & percentages of
+// students like:
+
+// var student_names = ["Farhan", "Asad", "Daud"];
+//     var score = [40, 40, 35];
+//     var total_marks = 60;
+
+//     for (var i = 0; i < student_names.length; i++) {
+//       var percentage = ((score[i] / total_marks) * 100).toFixed(2);
+//       document.write("Score of " + student_names[i] + " is " + score[i] + ". Percentage: " + percentage + "%<br>");
+//     }
+
+
+
+// var names=["Farhan","Daud","Usama","Asad","Ali","numan","Naveed"];
+// var marks=[50,45,35,40,47,37,55];
+// var total=60;
+// // var percentage=((marks[i]/total)*100).toFixed(2);
+
+
+
+
+// for(i=0; i<names.length ; i++){
+//     var percentage= ((marks[i]/total)*100).toFixed(2);
+//     document.write("Marks of  "+names[i]+" is "+marks[i]+" and "+". percentage:"+ percentage + "%<br>");
+// }
+
+
+
+// var color_names=[];
+// var userInput=prompt("what color you wants to add to the beginning ");
+// color_names.unshift(userInput);
+// document.write(color_names)
+
+
+
+    

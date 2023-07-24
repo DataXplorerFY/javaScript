ee6ad1bbabe9115fe057ffd33366c38cf9b71de9
Farhan Ali <farhanyounas5204@gmail.com>
2023-07-24 23:21:22 +0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,17 @@
+// Declaration of array
+var foods=["Pizza","burger","snacks","fries"];
+// Accessing the elements of an array
+console.log(foods[3]);
+// add or update the elements in array
+var names=[];
+names[0]="Farhan"
+names[1]="Ali"
+names[2]="Asad"
+names[3]="Nouman"
+names[4]="Raza"
+names[0]="Leo"
+names.push("Mahira Khan");
+names.push("aqsa","mueed","abdulrehman");
+
+console.log(names);
+

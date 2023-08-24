52df1fac9ade5813714419fbefdadeeb331d5edd
Farhan Ali <farhanyounas5204@gmail.com>
2023-08-24 20:56:32 +0500

    Add files via upload

---
+++ prob.js
@@ -0,0 +1,72 @@
+"use strict";
+// Write a function that checks if a given year is a leap year. A leap year is divisible by 4 but not divisible by 100, except when it is divisible by 400.
+function leapyear(num) {
+    if ((num % 4 == 0 && num % 100 != 0) || num % 400 == 0) {
+        console.log("The entered year is a leap year!");
+    }
+    else {
+        console.log("The entered year is not a leap year!");
+    }
+}
+leapyear(1901);
+// Create a function that takes a string as input and returns the string reversed.
+function reverse_String(String) {
+    var rev = String.split('').reverse().join('');
+    console.log(rev);
+}
+reverse_String("leo");
+// Write a function that calculates the average of all numbers in an array and returns the result.
+function avg(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    var Avg = sum / array.length;
+    return Avg;
+}
+var result = avg([1, 2, 3, 4, 5, 6]);
+console.log("The Average of given Array is ", result);
+// Create a function that takes a base and an exponent as parameters and returns the result of raising the base to the exponent.
+function exponentPower(base, exponent) {
+    var result = Math.pow(base, exponent);
+    return result;
+}
+var output = exponentPower(2, 4);
+console.log("The raising the base to the exponent is: ", output);
+// Write a function that counts the number of vowels (a, e, i, o, u) in a given string and returns the count.
+function vowels_Count(String) {
+    var sum = 0;
+    var lowerCaseString = String.toLowerCase();
+    for (var i = 0; i < String.length; i++) {
+        var character = lowerCaseString[i];
+        if (character == "a" || character == "e" || character == "i" || character == "o" || character == "u") {
+            sum++;
+        }
+    }
+    return sum;
+}
+var FinalResult = vowels_Count("Hey i am Farhan younas A passionate Data Scientist");
+console.log("The number of vowels in your string is ", FinalResult);
+// Create a function that checks if a given number is prime (only divisible by 1 and itself) and returns true or false.
+function primeNumberChecker(num) {
+    if (num <= 1) {
+        console.log("Not a Prime number");
+        return;
+    }
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            console.log("Not a Prime number");
+            return;
+        }
+    }
+    console.log("Prime number");
+}
+function total(arr) {
+    var sum = 0;
+    for (let i = 0; i < arr.length; i++) { // Change i <= arr.length to i < arr.length
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+var k = total([3, 9, 2, 6, 4, 9, 5]);
+console.log(k);
